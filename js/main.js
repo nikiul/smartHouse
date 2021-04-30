0d877dd1b6a0c07f77c20570966a4199bfdbb305
nikiul <nikulans@gmail.com>
2021-05-01 01:30:35 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// бургер
+const headerBurger = document.querySelector('.header__burger');
+const burgerIcon = document.querySelector('.burger__icon')
+const headerMenu = document.querySelector('.header__hide-menu')
+const headerMenuBg = document.querySelector('.header__hide-menu--bg')
+const bodyLock = document.querySelector('body')
+
+// Клик по кнопке для скрытия / показа фильтра и изменения  иконки
+headerBurger.onclick = function(){
+    burgerIcon.classList.toggle('burger__icon--active');
+    headerMenu.classList.toggle('header__hide-menu--active');
+    headerMenuBg.classList.toggle('header__hide-menu--bg--active');
+    bodyLock.classList.toggle('body__lock');
+};
+
+const listItem = document.querySelectorAll('.list__item');
+if(listItem) {
+    for (let list of listItem) {
+        const itemLink = list.querySelector('.list__item-link');
+
+        if(itemLink) {
+            itemLink.addEventListener('click', function(){
+                burgerIcon.classList.remove('burger__icon--active');
+                headerMenu.classList.remove('header__hide-menu--active');
+                headerMenuBg.classList.remove('header__hide-menu--bg--active');
+                bodyLock.classList.remove('body__lock');
+            })
+        }
+            
+    }
+}
+
+// карусель
+$('.owl-carousel').owlCarousel({
+    center: true,
+    stagePadding: 200,
+    items:3,
+    loop:true,
+    nav:true,
+    pagination: true,
+    margin:30,
+    responsive:{
+        0:{
+            items:2
+        }
+    }
+})
